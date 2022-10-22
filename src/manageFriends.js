4f49ed7d1b2c4c8bdc4be66f01689c70e1db6a54
OnanThairu <thairuonan@gmail.com>
2022-10-22 14:04:04 +0300

    chore: Add manageFriends function

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,12 @@
 export function manageFriends(state, action) {
   // your code here
+  switch(action.type) {
+    case "friends/add":
+      return { friends: [...state.friends, action.payload]}
+    case "friends/remove":
+      const newState = state.friends.filter(friend => friend.id === action.id)
+      return { friends: [...newState]}
+    default:
+      return state
+  }
 }
